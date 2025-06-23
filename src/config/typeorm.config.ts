da1433628b663f8f5c4ed3681a3c847c27f4ae71
Griss819 <rafaeliglesias819@gmail.com>
2025-06-23 13:20:02 -0400

    update env variable names

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -8,24 +8,12 @@ const configService = new ConfigService();
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: configService.get<string>('DB_HOST'),
-    port: configService.get<number>('DB_PORT'),
-    username: configService.get<string>('DB_USERNAME'),
-    password: configService.get<string>('DB_PASSWORD'),
-    database: configService.get<string>('DB_DATABASE'),
+    host: configService.get<string>('DATABASE_HOST'),
+    port: configService.get<number>('DATABASE_PORT'),
+    username: configService.get<string>('DATABASE_USER'),
+    password: configService.get<string>('DATABASE_PASSWORD'),
+    database: configService.get<string>('DATABASE_NAME'),
     entities: [__dirname + '/../entities/**/*.entity{.ts,.js}'],
     migrations: [__dirname + '/../migrations/*{.ts,.js}'],
     synchronize: false,
 });
-
-// export const AppDataSource = new DataSource({
-//     type: 'postgres',
-//     host: 'localhost',
-//     port: 5432,
-//     username: 'pocketAdmin',
-//     password: 'Waydims33241213',
-//     database: 'pocket-money-bd',
-//     entities: [__dirname + '../entities/**/*.entity{.ts,.js}'],
-//     migrations: [__dirname + '/../migrations/*{.ts,.js}'],
-//     synchronize: false,
-// });
