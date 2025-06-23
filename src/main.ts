55181a7a2b600b1e9f12030d3a813aa2b6f0a218
Griss819 <rafaeliglesias819@gmail.com>
2025-06-23 13:15:06 -0400

    trying to deploy

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from "node:process";
+import {ConfigService} from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+
   await app.listen(process.env.PORT ?? 3000);
 
   if (process.env.NODE_ENV === 'development') {
     app.enableCors();
   } else {
     app.enableCors({
-      origin: 'https://tudominio.com',
+      origin: process.env.DOMAIN_URL,
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       allowedHeaders: 'Content-Type,Authorization',
       credentials: true,
